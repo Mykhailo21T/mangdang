af0fb15a843589b86ee9b847b4b2d8499bb155e8
MathiasSThomsen <eaamatst@students.eaaa.dk>
2022-09-29 10:55:14 +0200

    Mathias commit

--- src/GUI.java
+++ src/GUI.java
@@ -1,8 +1,5 @@
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -290,7 +287,9 @@ public void udprint () {
 		players.add(me);
 		fields[xPos][yPos].setGraphic(new ImageView(hero_up));
 		return me;
+
 	}
+
 
 }
 
